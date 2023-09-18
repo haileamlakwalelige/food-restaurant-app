9b4d3351c7fca313bae88be05f25e9825ce882ff
Haileamlak Waleligne <120473611+haileamlakwalelige@users.noreply.github.com>
2023-09-18 14:29:52 +0300

    Update Hero Section

--- app/components/hero/Hero.js
+++ app/components/hero/Hero.js
@@ -8,8 +8,8 @@ const Hero=()=>{
     return(
         <div className="py-10 pt-72 lg:pt-0 bg-[#080622] h-screen flex item-center justify-center shadow-2xl shadow-white overflow-x-hidden "> 
          <div className='text-white flex items-center flex-col lg:flex-row justify-around w-full'>
-         <div className='bg-[#080622] cursor-pointer mt-96 lg:mt-auto text-center'>
-                <h1 className='font-bold text-3xl lg:text-5xl font-serif py-5 mt-20 lg:mt-auto  text-white cursor-pointer'>Kingo Restaurant</h1>
+         <div className='bg-[#080622] cursor-pointer mt-26 lg:mt-auto text-center'>
+                <h1 className='font-bold text-3xl lg:text-5xl font-serif py-5 mt-2 lg:mt-auto  text-white cursor-pointer'>Kingo Restaurant</h1>
                 <p className="font-semibold text-white hover:text-yellow-500 py-5 cursor-pointer">Our Food is Special, not because  its our food,<br/> because it is our hard word and passion</p>
                 <p className="font-semibold font-serif text-green-500 hover:text-yellow-500 py-5 cursor-pointer">Test The Hard work and passion</p>
             </div>
