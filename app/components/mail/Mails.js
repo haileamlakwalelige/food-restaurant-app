6e7c4e9450e895b9e37663d92ab589634b49bbff
HaileamlakWaleligne <hailemalakwaleligne3910@gmail.com>
2023-09-18 23:07:33 +0300

    updating the mail

--- app/components/mail/Mails.js
+++ app/components/mail/Mails.js
@@ -17,12 +17,12 @@ const Mails=()=>{
         <div className="bg-[#080622] font-serif">
             <div className="mt-32 overflow-x-hidden sm:overflow-x-visible">
             <h1 className="font-bold text-3xl py-10 flex justify-center items-center text-center underline">Our Foods</h1>
-                <div className='overflow-x-hidden placeholder:flex flex-col lg:flex-row justify-evenly pb-10 items-center'>
-                <di data-aos="fade-down"v className="border-gray-300 border-4 p-4 rounded-3xl my-5 h-[400px] flex flex-col justify-center items-center sm:w-72">
+                {/* <div className='overflow-x-hidden placeholder:flex flex-col lg:flex-row justify-evenly pb-10 items-center'>
+                <div data-aos="fade-down" className="border-gray-300 border-4 p-4 rounded-3xl my-5 h-[400px] flex flex-col justify-center items-center sm:w-72">
                     <Image src={image1} alt="First Images"  className="hover:brightness-50"
                     width={200} height={200} />
                     <p className="text-center py-2">Delicious Food</p>
-                </di>
+                </div>
                 <div data-aos="zoom-out" className="overflow-x-hidden border-gray-300 border-4 p-4 rounded-3xl my-5 h-[400px] flex flex-col justify-center items-center sm:w-72">
                     <Image src={image} alt="First Images"  className="hover:brightness-50"
                     width={200} height={200} />
@@ -32,6 +32,23 @@ const Mails=()=>{
                     <Image src={image2} alt="First Images"  className="hover:brightness-50"
                     width={200} height={200} />
                     <p className="text-center py-2">Amazing Food</p>
+                </div>
+                </div> */}
+                <div className='flex flex-col lg:flex-row justify-evenly pb-10 items-center'>
+                <div data-aos="fade-down" className="border-gray-300 border-4 p-4 rounded-3xl my-5 h-[400px] flex flex-col justify-center items-center sm:w-72">
+                    <Image src={image1} alt="First Images"  className="hover:brightness-50"
+                    width={200} height={200} />
+                    <p className="text-center py-2">Special Coffee</p>
+                </div>
+                <div data-aos="zoom-out" className="border-gray-300 border-4 p-4 rounded-3xl my-5 h-[400px] flex flex-col justify-center items-center sm:w-72">
+                    <Image src={image} alt="First Images"  className="hover:brightness-50"
+                    width={200} height={200} />
+                    <p className="text-center py-2">Super Food</p>
+                </div>
+                <div data-aos="zoom-in" className="border-gray-300 border-4 p-4 rounded-3xl my-5 h-[400px] flex flex-col justify-center items-center sm:w-72">
+                    <Image src={image2} alt="First Images"  className="hover:brightness-50"
+                    width={200} height={200} />
+                    <p className="text-center py-2">Special Food</p>
                 </div>
                 </div>
                 <div className='flex flex-col lg:flex-row justify-evenly pb-10 items-center'>
@@ -67,8 +84,7 @@ const Mails=()=>{
                     width={200} height={200} />
                     <p className="text-center py-2">Natural Food</p>
                 </div>
-                </div>
-                
+                </div>  
             </div>
         </div>
     )
