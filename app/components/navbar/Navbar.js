4930edad446956411341af7d57c281779d9dc079
HaileamlakWaleligne <hailemalakwaleligne3910@gmail.com>
2023-09-20 09:21:28 +0300

    updating the Navigation bar

--- app/components/navbar/Navbar.js
+++ app/components/navbar/Navbar.js
@@ -7,11 +7,11 @@ import {FaBars, FaTimes} from 'react-icons/fa';
 const Navbar=()=>{
   const [show, setShow]=useState(false);
     return(
-        <div className="text-black bg-white w-screen border-b-2-gray-500 lg:p-10 p-5 overflow-y-hidden shadow-2xl shadow-white ">
+        <div className="text-black bg-[#04080F] font-serif shadow-2xl shadow-gray-600 w-screen border-b-2-gray-500 lg:p-10 p-5 overflow-y-hidden shadow-2xl shadow-white ">
             <div className="grid grid-cols-3 gap-10 overflow-y-hidden cursor-pointer overflow-x-hidden ">
               <div className="font-bold text-blue-500 text-3xl lg:text-4xl hover:text-yellow-500">Kingo</div>
               <div className="hidden lg:block">
-                <ul className="flex font-bold justify-between">
+                <ul className="flex text-lg justify-between">
                     <li className="text-green-500 hover:text-orange-400">Home</li>
                     <li className="text-green-500 hover:text-orange-400">About Us</li>
                     <li className="text-green-500 hover:text-orange-400">Service</li>
@@ -20,7 +20,7 @@ const Navbar=()=>{
               </div>    
               {show ? (
               <div className="block h-screen overflow-y-hidden lg:hidden cursor-pointer">
-                <ul className="flex flex-col font-bold justify-between items-center">
+                <ul className="flex flex-col text-lg justify-between items-center">
                     <li className="text-green-500 py-10 hover:text-orange-400">Home</li>
                     <li className="text-green-500 py-10 hover:text-orange-400">About Us</li>
                     <li className="text-green-500 py-10 hover:text-orange-400">Service</li>
